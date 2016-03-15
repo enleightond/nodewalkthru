2659c5f9dc15f66704c09198b2c15875a2180f21
enleightond <enleightond@yahoo.com>
2016-03-14 18:19:17 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex');
+
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../views', 'index.html'))
+});
+
+
+module.exports = router;
